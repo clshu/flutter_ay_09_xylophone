7ebfc8978cc5f767805ccaba8bb5f630d1600692
clshu <ccss0630@yahoo.com>
2021-06-14 00:23:52 -0700

    1. Expanded buttons. 2. More refactoring code.

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,35 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  TextButton _createButton(MaterialColor color, int soundNumber) {
-    return TextButton(
-      onPressed: () {
-        _playSound(soundNumber);
-      },
-      child: Text(''),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(color),
+  Expanded _createExpandedButton(MaterialColor color, int soundNumber) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          _playSound(soundNumber);
+        },
+        child: Text(''),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+        ),
       ),
     );
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
             child: Column(
           children: [
-            _createButton(Colors.red, 1),
-            _createButton(Colors.orange, 2),
-            _createButton(Colors.yellow, 3),
-            _createButton(Colors.green, 4),
-            _createButton(Colors.blue, 5),
-            _createButton(Colors.indigo, 6),
-            _createButton(Colors.purple, 7),
+            _createExpandedButton(Colors.red, 1),
+            _createExpandedButton(Colors.orange, 2),
+            _createExpandedButton(Colors.yellow, 3),
+            _createExpandedButton(Colors.green, 4),
+            _createExpandedButton(Colors.blue, 5),
+            _createExpandedButton(Colors.indigo, 6),
+            _createExpandedButton(Colors.purple, 7),
           ],
         )),
       ),
