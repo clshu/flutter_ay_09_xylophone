c6b8d3f5126085d9aa6adae4e34b7a91b70f099b
clshu <ccss0630@yahoo.com>
2021-06-14 00:03:13 -0700

    1. Create TextButton 2. Create onPressed handler to play sound when button is pressed. 3. Use AudioCached class for local assets sound files. 4. Refactor the code to create 7 buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void _playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  TextButton _createButton(MaterialColor color, int soundNumber) {
+    return TextButton(
+      onPressed: () {
+        _playSound(soundNumber);
+      },
+      child: Text(''),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(color),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+          children: [
+            _createButton(Colors.red, 1),
+            _createButton(Colors.orange, 2),
+            _createButton(Colors.yellow, 3),
+            _createButton(Colors.green, 4),
+            _createButton(Colors.blue, 5),
+            _createButton(Colors.indigo, 6),
+            _createButton(Colors.purple, 7),
+          ],
+        )),
       ),
     );
   }
